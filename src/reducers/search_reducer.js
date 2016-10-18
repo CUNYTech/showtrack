311ef7c4a389db68f80f0f53c036d13c1dbea9a4
Jason Deng <jdeng1234@gmail.com>
2016-10-17 21:23:34 -0400

    Setup

--- src/reducers/search_reducer.js
+++ src/reducers/search_reducer.js
@@ -1,9 +1,11 @@
-import { SEARCH_RESULTS } from '../actions/types';
+import { SEARCH_RESULTS, FETCH_SHOW } from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
   case SEARCH_RESULTS:
     return { ...state, searchResults: action.payload.data };
+  case FETCH_SHOW:
+    return { ...state, show: action.payload.data };
   }
   return state;
 }
