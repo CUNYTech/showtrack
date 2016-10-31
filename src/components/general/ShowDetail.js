8d5f11eebb3136dba0ac168ce18892b838d67daa
Junjie Chen <junjie.chen18@gmail.com>
2016-10-30 20:14:07 -0700

    Updated ShowDetail.js and style.css

--- src/components/general/ShowDetail.js
+++ src/components/general/ShowDetail.js
@@ -20,9 +20,7 @@ renderGenres(show) {
   if (genres.length > 0) {
     genreListing = genres.map(genre => {
       return (
-        <div>
-          <span key={genre}>{genre}</span>
-        </div>
+          <span key={genre}>{genre} </span>
       )
     })
   }
@@ -31,8 +29,6 @@ renderGenres(show) {
 
 render() {
   const { show } = this.props;
-
-
   if (!show) {
     return (
       <div>Loading...</div>
@@ -41,15 +37,34 @@ render() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-xs-12 col-sm-4 col-md-6 col-lg-6">
+        <h2 className="col-xs-12">{show.name}</h2>
+      </div>
+      <div className="row">
+        <div className="col-sm-3">
           <img src={show.image.original} className="img-responsive" alt={show.name} />
         </div>
-        <div className="col-xs-12 col-sm-8 col-md-6 col-lg-6">
-          <h3>{show.name}</h3>
-          <br />
+        <div className="col-sm-6">
           <p dangerouslySetInnerHTML={{__html: show.summary}} className="font-weight-normal font-italic"></p>
-          {this.renderGenres(show)}
-          <div className="text-xs-center rating">Rating: {show.rating.average || 0}/10</div>
+          <div className="text-right">
+            <div className="social-buttons">
+              <div>Share this on:</div>
+              <a id="facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.tvmaze.com%2Fshows%2F73%2F{show.name}" title="facebook"><i className="fa fa-facebook-square fa-2x"></i></a>
+              <a id="twitter" target="_blank" href="https://twitter.com/home?status=http%3A%2F%2Fwww.tvmaze.com%2Fshows%2F73%2F{show.name}" title="twitter"><i className="fa fa-twitter-square fa-2x"></i></a>
+              <a id="reddit" target="_blank" href="//www.reddit.com/submit?url=http%3A%2F%2Fwww.tvmaze.com%2Fshows%2F73%2F{show.name}" title="reddit"><i className="fa fa-reddit-square fa-2x"></i></a>
+              <a id="tumblr" target="_blank" href="http://www.tumblr.com/share/link?url=http%3A%2F%2Fwww.tvmaze.com%2Fshows%2F73%2F{show.name}" title="tumblr"><i className="fa fa-tumblr-square fa-2x"></i></a>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-3">
+          <h3>Show Info</h3>
+          <div>Airs on: {show.network.country.code} {show.network.name} ({show.premiered})</div>
+          <div>Scheduled: {show.schedule.days[0]} at {show.schedule.time}</div>
+          <div>Status: {show.status}</div>
+          <div>Show Type: {show.type}</div>
+          <div>Genres: {this.renderGenres(show)}</div>
+          <div>Episodes ordered: {show.type}</div>
+          <br />
+          <div>Rating: {show.rating.average || 0}/10</div>
         </div>
       </div>
     </div>
