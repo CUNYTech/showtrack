dda9b57ec2d7e6bf1678ea97ceb717331373b9d1
David-Bohl <davebohl@gmail.com>
2016-11-03 18:16:15 -0400

    add super basic slider with placeholder image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,32 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import Slider from 'react-slick';
+import MultipleItems from 'react-slick';
+
 
 class Home extends Component {
   componentWillMount() {
     //this.props.fetchMessage();
   }
+
+  
+
+
   render() {
     return (
-      <div>Home page</div>
+      <div className='container'>
+        <Slider>
+          <div><img src='http://placekitten.com/g/400/200' /></div>
+          <div><img src='http://placekitten.com/g/400/200' /></div>
+        </Slider>
+
+      </div>
     )
   }
+
+
+
 }
 
 export default connect(null, actions)(Home);
