441aa134478a48d243704f959a8253cd6846657f
David-Bohl <davebohl@gmail.com>
2016-11-09 19:46:26 -0500

    move trending shows to landing page

---
+++ src/components/MyList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Hero
+} from "neal-react";
+
+export default (props) => {
+  return (
+    <Hero className="text-xs-center">
+        <h1 className="display-4"> Show Track </h1>
+        <p className="lead"> THE ONLINE TV SHOW COMMUNITY </p>
+      </Hero>
+  )
+}
