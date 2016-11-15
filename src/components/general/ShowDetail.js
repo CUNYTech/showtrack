79aaad27c90abfec40fe51d38e92313a32fd9bb9
Junjie Chen <junjie.chen18@gmail.com>
2016-11-15 15:54:01 -0800

    Updated ShowDetail.js and style.css

--- src/components/general/ShowDetail.js
+++ src/components/general/ShowDetail.js
@@ -37,7 +37,7 @@ render() {
   return (
     <div className="container">
       <div className="row">
-        <h2 className="col-xs-12">{show.name}</h2>
+        <h2 className="col-sm-12">{show.name}</h2>
       </div>
       <div className="row">
         <div className="col-sm-3">
@@ -66,6 +66,50 @@ render() {
           <br />
           <div>Rating: {show.rating.average || 0}/10</div>
         </div>
+      </div>
+      <div className="row" id="next-episode-widget">
+        <header className="col-sm-12 columns">
+          <h2>Next Episode</h2>
+        </header>
+        <div className="col-sm-12 columns">
+          <time datetime="2016-11-20" className="icon pull-left">
+            <strong>Nov</strong>
+            <span>20</span>
+            <em>Sun</em>
+          </time>
+          <div className="header-wrap">
+            <h3>
+              <a href="/episodes/895794/the-walking-dead-7x03-the-cell">The Cell</a>
+            </h3>
+            <h4>
+              Episode 7x05; Nov 20, 2016
+            </h4>
+          </div>
+          <p>A new group of survivors seem to have it all in their impressive community; however, there is a price.</p>
+          <a href="/videos/5480/the-walking-dead-7x03-the-cell-trailer"><i class="fa fa-film fa-lg"></i> Watch Trailer</a>
+        </div>
+      </div>
+      <div class="row">
+        <section class="small-12 medium-12 large-10 columns left" id="episode-list-short">
+          <h2>Previous Episodes</h2>
+          <div id="w0" class="grid-view">
+            <table class="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th>Episode Name</th>
+                  <th>Airdate</th>
+                  <th>Trailer</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr data-key="895795"><td>7x04: <a href="/episodes/895795/the-walking-dead-7x04-service">Service</a></td><td>Nov 13, 2016</td><td></td></tr>
+                <tr data-key="895794"><td>7x03: <a href="/episodes/895794/the-walking-dead-7x03-the-cell">The Cell</a></td><td>Nov 6, 2016</td><td><a class="fa fa-film fa-lg" href="/videos/5480/the-walking-dead-7x03-the-cell-trailer"></a></td></tr>
+                <tr data-key="895793"><td>7x02: <a href="/episodes/895793/the-walking-dead-7x02-the-well">The Well</a></td><td>Oct 30, 2016</td><td><a class="fa fa-film fa-lg" href="/videos/5385/the-walking-dead-7x02-the-well-trailer"></a></td></tr>
+              </tbody>
+            </table>
+          </div>
+          <a href="/shows/73/the-walking-dead/episodes">View full episode list »</a>
+        </section>
       </div>
     </div>
   )
