6074fc36979a7200d10c682ff7a8e7b901cf7d1e
Jason Deng <jdeng1234@gmail.com>
2016-11-30 01:38:06 -0500

    added draft for watch list

--- src/components/list/WatchListElement.js
+++ src/components/list/WatchListElement.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import { Link } from 'react-router';
 
 class WatchListElement extends Component {
 
@@ -11,6 +14,47 @@ class WatchListElement extends Component {
   renderProgress() {
     let episodesPerSeason = this.props.episodesPerSeason[this.props.show.show_id];
     console.log(this.props.show.show_details.content.name, episodesPerSeason);
+    console.log(this.props.show);
+    var options = [
+      { value: '1', label: '1' },
+      { value: '2', label: '2' }
+    ];
+
+    return (
+      <div>
+        <Select
+            name="form-field-name"
+            value="one"
+            options={options}
+            onChange={this.logChange}
+            className="col-md-2"
+        />
+        <Select
+          name="form-field-name"
+          value="one"
+          options={options}
+          onChange={this.logChange}
+          className="col-md-2"
+        />
+      <button type="button" className="btn btn-default btn-md"> Save </button>
+      </div>
+    )
+  }
+
+
+     logChange(val) {
+        console.log("Selected: " + val);
+    }
+
+    renderImage() {
+      return (
+        !this.props.show.show_details.content.image.medium ?
+          <img className='img-rounded' src='http://placehold.it/210x295' role="presentation"/>
+        :
+        <div>
+          <img className='img-rounded' src={this.props.show.show_details.content.image.medium} role="presentation"/>
+        </div>
+      )
   }
 
   render() {
@@ -22,13 +66,16 @@ class WatchListElement extends Component {
 
     let show = this.props.show.show_details.content;
     return (
-      <div>
-        Name: {show.name}
-        <br/>
-        Season: {this.props.show.progress.season}
-        Episode: {this.props.show.progress.episode}
-        {this.renderProgress()}
-
+      <div className="well">
+        <div className="row" style={{ marginBottom: 10 }}>
+          <h4 className="col-md-6 text-center">{show.name}</h4>
+        </div>
+        <div className="row">
+          <div className="col-md-6 text-center">{this.renderImage()}</div>
+            <p className="col-md-6" dangerouslySetInnerHTML={{__html: show.summary}} ></p>
+            <Link to={"/shows/"+ this.props.show.show_id}>View more details</Link>
+          {this.renderProgress()}
+        </div>
       </div>
     )
   }
--- src/components/search/ShowListing.js
+++ src/components/search/ShowListing.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-
 import Show from './Show';
 
 class ShowListing extends Component {
