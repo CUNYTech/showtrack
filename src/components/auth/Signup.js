daad16a2a6c28d309a55cec0b1e40457fab1ddeb
David-Bohl <davebohl@gmail.com>
2016-12-01 19:06:45 -0500

    style registration page, add background image

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -4,9 +4,18 @@ import * as actions from '../../actions'
 
 class Signup extends Component {
 
+  componentWillMount(){
+    document.body.setAttribute('id', 'signup');
+  }
+  componentWillUnmount(){
+    document.body.removeAttribute('id', 'signup');
+  }
+
   handleFormSubmit(formProps) {
     this.props.signupUser(formProps);
   }
+
+
 
   renderAlert() {
     if(this.props.errorMessage) {
@@ -22,42 +31,50 @@ class Signup extends Component {
 
     return (
       <div className="half">
-      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <fieldset className="form-group">
-          <label>Email Address</label>
-          <input {...email} type="email" placeholder="Enter email" className="form-control" />
-          <small id="emailHelp" className="form-text text-muted">We will never share your email with anyone else.</small>
-          {email.touched && email.error && <div className="error"><strong>{email.error}</strong></div>}
-        </fieldset>
+        <div className="row">
+          <div className="col-sm-4"></div>
+            <div className="col-sm-6 well">
+              <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <fieldset className="form-group">
+                  <label>Email Address</label>
+                  <input {...email} type="email" placeholder="Enter email" className="form-control" />
+                  <small id="emailHelp" className="form-text text-muted">We will never share your email with anyone else.</small>
+                  {email.touched && email.error && <div className="error"><strong>{email.error}</strong></div>}
+                </fieldset>
 
-        <fieldset className="form-group">
-          <label>Display Name</label>
-          <input {...display_name} type="text" placeholder="Enter your display name" className="form-control" />
-          {display_name.touched && display_name.error && <div className="error"><strong>{display_name.error}</strong></div>}
-        </fieldset>
+                <fieldset className="form-group">
+                  <label>Display Name</label>
+                  <input {...display_name} type="text" placeholder="Enter your display name" className="form-control" />
+                  {display_name.touched && display_name.error && <div className="error"><strong>{display_name.error}</strong></div>}
+                </fieldset>
 
-        <fieldset className="form-group">
-          <label>Username</label>
-          <input {...username} type="text" placeholder="Enter username" className="form-control" />
-          {username.touched && username.error && <div className="error"><strong>{username.error}</strong></div>}
-        </fieldset>
+                <fieldset className="form-group">
+                  <label>Username</label>
+                  <input {...username} type="text" placeholder="Enter username" className="form-control" />
+                  {username.touched && username.error && <div className="error"><strong>{username.error}</strong></div>}
+                </fieldset>
 
-        <fieldset className="form-group">
-          <label>Password</label>
-          <input {...password} type="password" placeholder="Enter password" className="form-control" />
-          {password.touched && password.error && <div className="error"><strong>{password.error}</strong></div>}
-        </fieldset>
+                <fieldset className="form-group">
+                  <label>Password</label>
+                  <input {...password} type="password" placeholder="Enter password" className="form-control" />
+                  {password.touched && password.error && <div className="error"><strong>{password.error}</strong></div>}
+                </fieldset>
 
-        <fieldset className="form-group">
-          <label>Confirm Password</label>
-          <input {...passwordConfirm} type="password" placeholder="Confirm password" className="form-control" />
-          {passwordConfirm.touched && passwordConfirm.error && <div className="error"><strong>{passwordConfirm.error}</strong></div>}
+                <fieldset className="form-group">
+                  <label>Confirm Password</label>
+                  <input {...passwordConfirm} type="password" placeholder="Confirm password" className="form-control" />
+                  {passwordConfirm.touched && passwordConfirm.error && <div className="error"><strong>{passwordConfirm.error}</strong></div>}
 
-        </fieldset>
-        {this.renderAlert()}
-        <button action="submit" className="btn btn-primary pull-right">Register</button>
+                </fieldset>
+                {this.renderAlert()}
+                <button action="submit" className="btn btn-primary pull-right">Register</button>
 
-      </form>
+              </form>
+            </div>
+          <div className="col-sm-2"></div>
+
+        </div>
+
       </div>
     );
   }
